86a30e53eb35cbe9085306ff2d20ed8630be2c55
Shamim Ahmed Raju <140370926+ShamimAhmed510@users.noreply.github.com>
2026-05-24 01:14:40 +0600

    Add files via upload

---
+++ DFS.cpp
@@ -0,0 +1,53 @@
+// Subtree Size Calculation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5+5;
+vector<int>adj[N];
+int visited[N];
+int depth[N];
+int subtree_sz[N];
+
+void dfs(int node)
+{
+    // if(visited[node]) return;
+    visited[node] = 1;
+    // cout<<node<<endl;
+    subtree_sz[node] = 1;
+    // int tot = 0;
+    for(auto u:adj[node])
+    {
+        if(visited[u]==0)
+        {
+            depth[u] = depth[node]+1;
+            dfs(u);
+            // tot += subtree_sz[u];
+            subtree_sz[node] += subtree_sz[u];
+        }
+
+    }
+
+    // subtree_sz[node] = tot+1;
+
+
+}
+
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    for(int i=0; i<m; ++i)
+    {
+        int u,v;
+        cin>>u>>v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    // for(auto u:adj[1]) cout<<u<<' ';
+
+    dfs(9);
+    for(int i=1; i<=n; ++i) cout<<i<<" -> "<<subtree_sz[i]<<endl;
+}
